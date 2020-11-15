fa37143496eca1f4048d34188cf7087b1550fc3d
rquiroz3290 <rquiroz3290@gmail.com>
2020-11-14 22:22:47 -0500

    built out the questions (still need to finish editing for typos and provide real data but it functions) also have a functioning inquirer module and succesfully able to use the markdown module to output md data

--- index.js
+++ index.js
@@ -1,16 +1,76 @@
+///dependencies///
+const inquirer = require('inquirer')
+const fs = require("fs")
+const generateMarkdown = require('./utils/generateMarkdown')
+
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        name: 'title',
+        message: "What is the title of your Project?"
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: "Please enter a description for your project?"
+    },
+    {
+        type: 'input',
+        name: 'installInstructions',
+        message: "What are the installation instructions for your project?"
+    },
+    {
+        type: 'input',
+        name: 'usageInfo',
+        message: "Please enter in usage information.",
+    },
+    {
+        type: 'list',
+        name: 'contributionGuidelines',
+        message: "What guidlines would you like to set for contribution?",
+        choices: ["To report bugs or suggest enhancements please use my contact info listed further down below", "Code of Conduct: all communication should be friendly and professional, any deviation from these guidelines will result in termination of privileges."]
+    },
+    {
+        type: 'input',
+        name: 'testingInstructions',
+        message: 'Please enter in instructions for testing'
+    },
+    {
+        type: 'list',
+        name: 'licenses',
+        message: "Please select a license you would like to use.",
+        choices: ["To report bugs or suggest enhancements please use my contact info listed further down below", "Code of Conduct: all communication should be friendly and professional, any deviation from these guidelines will result in termination of privileges."]
+    },
+    {
+        type: 'input',
+        name: 'userGitName',
+        message: "What is your GitHub username?"
+    },
+    {
+        type: 'input',
+        name: 'userEmail',
+        message: "What is your email?"
+    },
 ];
 
 // function to write README file
+
 function writeToFile(fileName, data) {
 }
 
 // function to initialize program
 function init() {
+    console.log("Initializing Program")
 
+    inquirer.prompt(questions).then((data) => {
+        //console.log(data)
+        let fileName = `${data.title.toLowerCase().split(' ').join('')}.ReadMe.md`
+        let output = generateMarkdown(data)
+        //writeToFile(fileName, data);
+      
+    })
 }
-
 // function call to initialize program
+
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,9 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  # ${data.description}
+
+  ${data.installInstructions}
 
 `;
 }
